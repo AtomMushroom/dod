69d0e2c8eb699696eb1df41f5371f6f1c46d1658
Adam Nobody <93332403+adamnobody@users.noreply.github.com>
2022-11-11 11:16:54 +0300

    Add files via upload

---
+++ Client/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Windows.Forms;
+using Client.Chat;
+
+namespace Client
+{
+    public partial class Form1 : Form, IServiceCallback
+    {
+        bool isConnected = false;
+        ServiceClient client;
+        int ID;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void ConnectUser()
+        {
+            if (!isConnected)
+            {
+                client = new ServiceClient(new InstanceContext(this));
+                
+                ID = client.Connect(textBox1.Text);
+                textBox1.Enabled = false;
+                isConnected = !isConnected;
+                button1.Text = "Отключиться";
+            }
+        }
+
+        void DisconnectUser()
+        {
+            if (isConnected)
+            {
+                client.Disconnect(ID);
+                client = null;
+                textBox1.Enabled = true;
+                isConnected = !isConnected;
+                button1.Text = "Подключиться";
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                DisconnectUser();
+            }
+            else
+            {
+                ConnectUser();
+            }
+        }
+
+        public void MessageCallback(string message)
+        {
+            listBox1.Items.Add(message);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DisconnectUser();
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if(client != null)
+                {
+                    client.SendMessage(textBox2.Text, ID);
+                    textBox2.Text = String.Empty;
+                }
+            }
+        }
+    }
+}
---
+++ kret/Service.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace kret
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class Service : IService
+    {
+        List<User> users = new List<User>();
+        int id = 1;
+
+        public int Connect(string name)
+        {
+            User user = new User
+            {
+                ID = id,
+                Name = name,
+                operationContext = OperationContext.Current
+            };
+
+            id++;
+
+            SendMessage($" {name} подключился.", 0);
+
+            users.Add(user);
+            return user.ID;
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = users.FirstOrDefault(i => i.ID == id);
+
+            if(user != null)
+            {
+                users.Remove(user);
+
+                SendMessage($" {user.Name} отключился.", 0);
+            }
+        }
+
+        public void SendMessage(string message, int id)
+        {
+            foreach(var user in users)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+
+                var usr = users.FirstOrDefault(i => i.ID == id);
+                if(usr != null)
+                {
+                    answer += $": {usr.Name} ";
+                }
+
+                answer += message;
+
+                user.operationContext.GetCallbackChannel<IServiceCallback>().MessageCallback(answer);
+            }
+        }
+    }
+}
